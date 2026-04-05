35607140d7a3345f0b12c2ae0eaf320aa98e8222
wassupmaneitswilliams <williamswilla0@gmail.com>
2026-04-06 01:28:36 +0400

    Update stats.c

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -14,8 +14,8 @@
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Shane Noble>
+ * @date <April 5th 2026 >
  *
  */
 
@@ -34,10 +34,95 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+  /* Main Function */
+
+
+    int size = sizeof(test) / sizeof(test[0]);
+
+    printf("Original Array:\n");
+    print_array(test, size);
+
+    sort_array(test, size);
+
+    printf("\nSorted Array (Descending):\n");
+    print_array(test, size);
+
+    printf("\nStatistics:\n");
+    print_statistics(test, size);
+
+    return 0;
+}
+
+/* Print statistics */
+void print_statistics(unsigned char *array, int size) {
+    printf("Maximum: %d\n", find_maximum(array, size));
+    printf("Minimum: %d\n", find_minimum(array, size));
+    printf("Mean: %d\n", find_mean(array, size));
+    printf("Median: %d\n", find_median(array, size));
+}
+
+/* Print array */
+void print_array(unsigned char *array, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
 }
 
-/* Add other Implementation File Code Here */
+/* Find maximum */
+unsigned char find_maximum(unsigned char *array, int size) {
+    unsigned char max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+/* Find minimum */
+unsigned char find_minimum(unsigned char *array, int size) {
+    unsigned char min = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+/* Find mean */
+unsigned char find_mean(unsigned char *array, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    }
+    return (unsigned char)(sum / size);
+}
+
+/* Find median */
+unsigned char find_median(unsigned char *array, int size) {
+    if (size % 2 == 0) {
+        int mid = size / 2;
+        return (unsigned char)((array[mid] + array[mid - 1]) / 2);
+    } else {
+        return array[size / 2];
+    }
+}
+
+/* Sort array (descending) using bubble sort */
+void sort_array(unsigned char *array, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                unsigned char temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
